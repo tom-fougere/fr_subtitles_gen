0fc4de200831d65fba6a29053b0795c6bbab75e5
Tom <fougeretom@gmail.com>
2023-05-18 16:08:27 +0200

    Create UTs

---
+++ test/test_rw_srt_file.py
@@ -0,0 +1,32 @@
+import unittest
+import os
+from src.rw_srt_file import *
+
+
+class TestFunctions(unittest.TestCase):
+    def test_read_srt(self):
+        df = read_srt("english_subtitles.srt")
+
+        self.assertEqual(len(df), 2)
+        self.assertEqual(len(df.keys()), 4)
+
+        for key, val in COLUMNS_NAME.items():
+            self.assertTrue(val in df.keys())
+
+    def test_write_srt_file(self):
+        new_file_path = 'new_file.srt'
+        df = read_srt("english_subtitles.srt")
+        write_srt_file(
+            df=df,
+            column_name_sentence=COLUMNS_NAME['sentence'],
+            output_file_path=new_file_path
+        )
+
+        df_translated = read_srt(new_file_path)
+        self.assertTrue(df.equals(df_translated))
+
+        os.remove(new_file_path)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test/test_translation.py
@@ -0,0 +1,68 @@
+import unittest
+
+import pandas as pd
+
+from src.translation import *
+
+
+class SRTranslationTest(unittest.TestCase):
+    def test_init(self):
+        srt_translator = SRTranslator()
+        self.assertEqual(srt_translator.target_language, "fr")
+        self.assertEqual(srt_translator.max_length, 512)
+        self.assertEqual(srt_translator.default_model, True)
+        self.assertEqual(srt_translator.model, "Helsinki-NLP/opus-mt-en-fr")
+        self.assertEqual(srt_translator.tokenizer, None)
+
+        srt_translator = SRTranslator(target_language='en', max_length=256)
+        self.assertEqual(srt_translator.target_language, "en")
+        self.assertEqual(srt_translator.max_length, 256)
+        self.assertEqual(srt_translator.default_model, True)
+        self.assertEqual(srt_translator.model, "Helsinki-NLP/opus-mt-en-en")
+        self.assertEqual(srt_translator.tokenizer, None)
+
+    def test_set_model(self):
+        srt_translator = SRTranslator()
+        srt_translator.set_model(model="test_model", tokenizer="test_tokenizer")
+
+        self.assertEqual(srt_translator.default_model, False)
+        self.assertEqual(srt_translator.model, "test_model")
+        self.assertEqual(srt_translator.tokenizer, "test_tokenizer")
+
+    def test_translate_sentence(self):
+        srt_translator = SRTranslator()
+        new_sentence = srt_translator.translate_sentence(sentence="Hello world")
+        self.assertEqual(new_sentence, "Bonjour monde")
+
+    def test_translate_df(self):
+        df = pd.DataFrame.from_dict({
+            'sentence': ['Hello world']
+        })
+
+        srt_translator = SRTranslator()
+        srt_translator.translate_df(
+            df=df,
+            column_to_translate='sentence',
+            column_translation=None,
+        )
+        self.assertTrue('sentence_translated' in df.keys())
+        self.assertEqual(df['sentence_translated'][0], 'Bonjour monde')
+
+        srt_translator.translate_df(
+            df=df,
+            column_to_translate='sentence',
+            column_translation='translation',
+        )
+        self.assertTrue('translation' in df.keys())
+        self.assertEqual(df['translation'][0], 'Bonjour monde')
+
+    def test_translate_file(self):
+        srt_translator = SRTranslator()
+        df = srt_translator.translate_file("english_subtitles.srt")
+
+        self.assertEqual(len(df), 2)
+        self.assertEqual(df['sentence_translated'][0], "Bonjour monde")
+
+
+if __name__ == '__main__':
+    unittest.main()
