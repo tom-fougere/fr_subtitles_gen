2df30b06eabe398a44e7cd431f0e70a70fac6bf3
Tom <fougeretom@gmail.com>
2023-05-18 15:27:22 +0200

    Create python scripts

---
+++ src/rw_srt_file.py
@@ -0,0 +1,35 @@
+import re
+import pandas as pd
+
+COLUMNS_NAME = {
+    'id': 'id',
+    'start_time': 'start_time',
+    'end_time': 'end_time',
+    'sentence': 'sentence',
+}
+
+
+def read_srt(srt_file_path):
+
+    # Read file
+    with open(srt_file_path, 'r') as f:
+        srt_data = f.read()
+
+    # Parse data
+    srt_regex = r'(\d+)\n(\d{2}:\d{2}:\d{2},\d{3}) --> (\d{2}:\d{2}:\d{2},\d{3})\n(.+?(?=\n\d|$))'
+    srt_matches = re.findall(srt_regex, srt_data, re.DOTALL)
+
+    # Extract each data
+    srt_data_list = [(int(m[0]), m[1], m[2], m[3].replace('\n', ' ')) for m in srt_matches]
+
+    return pd.DataFrame(srt_data_list, columns=list(COLUMNS_NAME.values()))
+
+
+def write_srt_file(df, column_name_sentence, output_file_path):
+    with open(output_file_path, 'w') as f:
+        for index, row in df.iterrows():
+
+            # Build each line
+            f.write(str(row[COLUMNS_NAME['id']]) + '\n')
+            f.write(row[COLUMNS_NAME['start_time']] + ' --> ' + row[COLUMNS_NAME['end_time']] + '\n')
+            f.write(row[column_name_sentence] + '\n\n')
---
+++ src/translation.py
@@ -0,0 +1,63 @@
+from transformers import pipeline
+from tqdm import tqdm
+from src.rw_srt_file import read_srt, COLUMNS_NAME
+
+
+class SRTranslator:
+    def __init__(self, target_language='fr', max_length=512):
+        self.target_language = target_language
+        self.max_length = max_length
+        self.default_model = True
+        self.model = f"Helsinki-NLP/opus-mt-en-{self.target_language}"
+        self.tokenizer = None
+
+    def set_model(self, model, tokenizer):
+        self.model = model
+        self.tokenizer = tokenizer
+        self.default_model = False
+
+    def translate_sentence(self, sentence):
+
+        # Simple pre-processing
+        sentence = f'{sentence.strip()}'
+
+        if self.default_model:
+            # Initialize the translation pipeline
+            translator = pipeline("translation", model=self.model)
+
+            # Translate the sentence
+            translated_sentence = translator(sentence, max_length=self.max_length)[0]['translation_text']
+
+        else:
+            # Add start and end of sentence tokens and tokenize the input
+            inputs = self.tokenizer(sentence, return_tensors='pt')
+
+            # Translate the input and decode the output
+            outputs = self.model.generate(**inputs)
+            translated_sentence = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+        return translated_sentence
+
+    def translate_df(self, df, column_to_translate, column_translation=None):
+
+        # Name of the new column
+        column_name = column_to_translate + '_translated' if column_translation is None else column_translation
+
+        # Apply the translation to each sentence in the DataFrame
+        tqdm.pandas()
+        df[column_name] = df[column_to_translate].progress_apply(
+            lambda x: self.translate_sentence(x))
+
+    def translate_file(self, srt_file_path):
+
+        # Read file
+        srt_df = read_srt(srt_file_path)
+
+        # Translate
+        self.translate_df(
+            df=srt_df,
+            column_to_translate=COLUMNS_NAME['sentence'],
+            column_translation=None,
+        )
+
+        return srt_df
